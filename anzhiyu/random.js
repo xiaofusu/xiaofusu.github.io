2b0077947732d24af279e70bcc00264abf967ed0
吴志怡 <3118732544@qq.com>
2023-11-15 12:26:00 +0800

    Site updated: 2023-11-15 12:26:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/14/hello-world/","2023/11/14/如何解决spring中的循环依赖/","2023/11/14/My-new-Post/"];function toRandomPost(){
+var posts=["2023/11/14/hello-world/","2023/11/14/My-new-Post/","2023/11/14/如何解决spring中的循环依赖/","2023/11/15/html知识点整理/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
