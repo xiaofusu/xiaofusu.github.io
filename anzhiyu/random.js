f033bcf82e8c692623bf9e670fc67792c14a0836
吴志怡 <3118732544@qq.com>
2023-11-25 18:02:07 +0800

    Site updated: 2023-11-25 18:02:07

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/17/BOM与DOM/","2023/11/22/JVM内存模型详解/","2023/11/25/String-StringBuffer-StringBuilder的区别/","2023/11/15/css常用样式/","2023/11/19/hashmap是如何解决hash冲突的？/","2023/11/14/hello-world/","2023/11/15/html知识点整理/","2023/11/21/java类加载过程/","2023/11/14/如何解决spring中的循环依赖/","2023/11/24/深入理解字符串常量池/"];function toRandomPost(){
+var posts=["2023/11/17/BOM与DOM/","2023/11/22/JVM内存模型详解/","2023/11/15/css常用样式/","2023/11/25/String-StringBuffer-StringBuilder的区别/","2023/11/19/hashmap是如何解决hash冲突的？/","2023/11/14/hello-world/","2023/11/15/html知识点整理/","2023/11/14/如何解决spring中的循环依赖/","2023/11/21/java类加载过程/","2023/11/24/深入理解字符串常量池/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
